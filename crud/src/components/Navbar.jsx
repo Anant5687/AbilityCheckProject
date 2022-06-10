c4dc86cb7313d6b106cd8fe7d4ea3f381c73096b
Anant Jindal <anantjindal45@gmail.com>
2022-06-10 20:35:23 +0530

    Project is finalised

---
+++ crud/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Button } from '@mui/material';
+import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+    const navigate = useNavigate()
+    const onlogout = () => {
+        localStorage.removeItem('token')
+        navigate('/')
+    }
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/home">CRUD App.</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link to="search" style={{ textDecoration: "none",  marginLeft:"75%" }}>
+                        <Button style={{ color: "green" }}>Search</Button>
+                    </Link>
+                    <Button style={{fontSize: "14px", color: "green" }} onClick={onlogout}>Logout</Button>
+                    
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
